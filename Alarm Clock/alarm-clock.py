8e10fca8f89241cb84e265bbbd6cf53ad95303c6
DurgeshKavate003 <durgesh.kavate@outlook.com>
2023-03-03 22:13:16 +0530

    commit - 01

---
+++ Alarm Clock/alarm-clock.py
@@ -0,0 +1,29 @@
+from playsound import playsound
+import time
+
+CLEAR = '\033[2J'
+CLEAR_AND_RETURN = '\033[H'
+
+def alarm(seconds):
+    time_elapsed = 0
+
+    print(CLEAR)
+
+    while time_elapsed < seconds:
+        time.sleep(1)
+        time_elapsed += 1
+
+        time_left = seconds - time_elapsed
+        minutes_left = time_left // 60
+        seconds_left = time_left % 60
+
+        print(f"{CLEAR_AND_RETURN}Time Left - {minutes_left:02d}:{seconds_left:02d}")
+
+    playsound('alarm.mp3')
+
+
+minutes = int(input('Alarm for How many Minutes: '))
+seconds = int(input('Alarm for How many Seconds: '))
+total_seconds = minutes * 60 + seconds
+
+alarm(total_seconds)
